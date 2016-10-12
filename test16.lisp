5b6efeb0e11fb60d1c2eaa96ef75c3d7a8d09812
kammitama5 <krystal.maughan@gmail.com>
2016-10-12 15:41:01 -0700

    1st commit

---
+++ test16.lisp
@@ -0,0 +1,16 @@
+; functions
+
+; average function 
+
+(defun averagenum (n1 n2 n3 n4)
+	(/ (+ n1 n2 n3 n4) 4)
+)
+
+(write(averagenum 10 20 30 40))
+
+; prints 25
+
+; how would you find average of any number?
+; defun of sum of elements 
+; defun of number of elements
+; divide both
