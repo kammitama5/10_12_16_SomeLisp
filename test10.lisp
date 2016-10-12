0449ed1fec68f9654b433dc8de033deb1afe1f12
kammitama5 <krystal.maughan@gmail.com>
2016-10-12 11:00:14 -0700

    1st commit

---
+++ test10.lisp
@@ -0,0 +1,9 @@
+; symbol value function
+
+(setq x 10)
+(setq y 20)
+(format t "x = ~2d y = ~2d ~%" x y) ; prints x = 10   y = 20
+
+(setq x 100)
+(setq y 200)
+(format t "x = ~2d y = ~2d" x y) ; prints x = 100  y = 200
