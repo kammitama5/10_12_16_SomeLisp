b21f080cf8227a84e9c6ad142c26891ec21ebe54
kammitama5 <krystal.maughan@gmail.com>
2016-10-12 09:21:32 -0700

    1st commit

---
+++ test5.lisp
@@ -0,0 +1,17 @@
+; defstruct functions
+; name of structure type becomes a valid type symbol
+
+(setq x 10)
+(setq y 34.567)
+(setq ch nil)
+(setq ch nil)
+(setq n 123.78)
+(setq bg 11.0e+4)
+(setq r 124/2)
+
+(print x) ; prints 10
+(print y) ; prints 34.567
+(print n) ; prints 123.78
+(print ch) ; prints NIL
+(print bg) ; prints 110000.0
+(print r) ; prints 62
